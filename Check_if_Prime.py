6a03097b616a74d327e416c043f0dcd316a1223e
fermihacker <57656949+fermihacker@users.noreply.github.com>
2020-07-04 10:20:58 +0530

    Update Check_if_Prime.py

--- Check_if_Prime.py
+++ Check_if_Prime.py
@@ -1,6 +1,10 @@
+from itertools import islice,count
+from math import floor,sqrt
+
 def prime(n):
     if n == 0 or n == 1:
         return None
     if n == 2:
         return True
     return all([not(n % i == 0) for i in islice(count(2),floor(sqrt(n)))])
+
